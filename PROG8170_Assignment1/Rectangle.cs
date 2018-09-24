b3ae701ba47df90eba67ed7e807879bd587f873f
uiassignment2 <37420621+uiassignment2@users.noreply.github.com>
2018-09-23 23:46:36 -0400

    Rectangle class added

---
+++ PROG8170_Assignment1/Rectangle.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PROG8170_Assignment1
+{
+    class Rectangle
+    {
+        private int length;
+        private int width;
+
+        public Rectangle()
+        {
+            this.length = 1;
+            this.width = 1;
+        }
+        public Rectangle(int length, int width)
+        {
+            this.length = length;
+            this.width = width;
+        }
+        public int GetLength()
+        {
+            return this.length;
+        }
+        public int SetLength(int length)
+        {
+            this.length = length;
+            return this.length;
+        }
+        public int GetWidth()
+        {
+            return this.width;
+        }
+        public int SetWidth(int width)
+        {
+            this.width = width;
+            return this.width;
+        }
+        public int GetPerimeter()
+        {
+            return 2 * (length + width);
+        }
+        public int GetArea()
+        {
+            return length * width;
+        }
+    }
+}
